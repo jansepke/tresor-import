eaac85d8dda41835cf2099352796c568032f335d
David Dreher <dredav@users.noreply.github.com>
2021-12-20 21:42:02 +0100

    Adjust parser for sample from #4880

--- src/brokers/sbroker27.js
+++ src/brokers/sbroker27.js
@@ -30,20 +30,69 @@ const parseBuyDocument = (/** @type {Importer.page} */ content) => {
   return validateActivity(activity);
 };
 
+const parseDividendDocument = (/** @type {Importer.page} */ content) => {
+  content = content.slice(content.indexOf('ISIN'));
+
+  const shares = findShares(content);
+  let amount = findAmountPayout(content);
+
+  /** @type {Partial<Importer.Activity>} */
+  let activity = {
+    broker: 'sBroker',
+    type: 'Buy',
+    isin: findIsin(content),
+    wkn: findWkn(content),
+    company: findCompany(content),
+    shares: +shares,
+    fee: findFee(content),
+  };
+
+  [activity.date, activity.datetime] = findDateTime(content);
+  [activity.foreignCurrency, activity.fxRate] = findForeignInformation(content);
+
+  if (!activity.foreignCurrency) {
+    delete activity.foreignCurrency;
+  }
+
+  if (!activity.fxRate) {
+    delete activity.fxRate;
+  } else {
+    amount = amount.div(activity.fxRate);
+  }
+
+  activity.amount = +amount;
+  activity.price = +amount.div(shares);
+  activity.tax = findTax(content, activity.fxRate || 1);
+
+  return validateActivity(activity);
+};
+
 const findShares = (/** @type {Importer.page} */ content) => {
   return Big(parseGermanNum(content[5]));
 };
 
+const findPositionOfIsin = (/** @type {Importer.page} */ content) => {
+  let position = content.indexOf('Handels-/Ausf');
+  if (position > 0) {
+    return position;
+  }
+
+  position = content.indexOf('Zahlbarkeitstag');
+  if (position > 0) {
+    return position;
+  }
+};
+
 const findCompany = (/** @type {Importer.page} */ content) => {
-  return content.slice(6, content.indexOf('Handels-/Ausf') - 2).join(' ');
+  return content.slice(6, findPositionOfIsin(content) - 2).join(' ');
 };
 
 const findIsin = (/** @type {Importer.page} */ content) => {
-  return content[content.indexOf('Handels-/Ausf') - 2];
+  return content[findPositionOfIsin(content) - 2];
 };
 
 const findWkn = (/** @type {Importer.page} */ content) => {
-  return content[content.indexOf('Handels-/Ausf') - 1].replace(/[{()}]/g, '');
+  return content[findPositionOfIsin(content) - 1].replace(/[{()}]/g, '');
 };
 
 const findFee = (/** @type {Importer.page} */ content) => {
@@ -62,6 +111,12 @@ const findAmount = (/** @type {Importer.page} */ content) => {
   return Big(parseGermanNum(content[content.indexOf('Kurswert') + 1]));
 };
 
+const findAmountPayout = (/** @type {Importer.page} */ content) => {
+  return Big(
+    parseGermanNum(content[content.indexOf('Dividendengutschrift') + 1])
+  );
+};
+
 const findDateTime = (/** @type {Importer.page} */ content) => {
   let dateValue, timeValue;
   let lineNumber = content.indexOf('Schlusstag/-Zeit');
@@ -74,6 +129,12 @@ const findDateTime = (/** @type {Importer.page} */ content) => {
 
     if (lineNumber > 0) {
       dateValue = content[lineNumber + 1];
+    } else {
+      lineNumber = content.indexOf('Zahlbarkeitstag');
+
+      if (lineNumber > 0) {
+        dateValue = content[lineNumber + 1];
+      }
     }
   }
 
@@ -85,9 +146,77 @@ const findDateTime = (/** @type {Importer.page} */ content) => {
   );
 };
 
+const findForeignInformation = (/** @type {Importer.page} */ content) => {
+  const lineNumber = content.indexOf('Devisenkurs');
+  if (lineNumber <= 0) {
+    return [undefined, undefined];
+  }
+
+  return [
+    content[lineNumber + 2].split(/\s/)[1],
+    parseGermanNum(content[lineNumber + 3]),
+  ];
+};
+
+const findTax = (
+  /** @type {Importer.page} */ content,
+  /** @type Number */ fxRate
+) => {
+  let total = Big(0);
+
+  // Withholding tax (1)
+  {
+    const taxLineNumber = content.indexOf('Einbehaltene');
+    if (
+      taxLineNumber >= 0 &&
+      taxLineNumber + 1 < content.length &&
+      content[taxLineNumber + 1] === 'Quellensteuer'
+    ) {
+      const withholdingAmount = Big(parseGermanNum(content[taxLineNumber + 4]));
+      total = total.add(+withholdingAmount.div(fxRate));
+    }
+  }
+
+  // Withholding tax (2)
+  {
+    const taxLineNumber = content.indexOf('Verrechnete');
+    if (
+      taxLineNumber >= 0 &&
+      taxLineNumber + 5 < content.length &&
+      content[taxLineNumber + 5] === 'Quellensteuer'
+    ) {
+      total = total.add(
+        parseGermanNum(content[taxLineNumber + 10].split(/\s/)[1])
+      );
+    }
+  }
+
+  // Kapitalertragsteuer
+  {
+    const taxLineNumber = content.indexOf('Kapitalertragsteuer');
+    if (taxLineNumber > 0) {
+      total = total.add(parseGermanNum(content[taxLineNumber + 3]));
+    }
+  }
+
+  // Solidaritätszuschlag
+  {
+    const taxLineNumber = content.indexOf('tszuschlag');
+    if (taxLineNumber > 0) {
+      total = total.add(parseGermanNum(content[taxLineNumber + 3]));
+    }
+  }
+
+  return +total;
+};
+
 const getDocumentType = (/** @type {Importer.page} */ content) => {
   if (isBuy(content)) {
     return 'Buy';
+  }
+
+  if (isDividend(content)) {
+    return 'Dividend';
   }
 };
 
@@ -103,6 +232,10 @@ const isBuy = (/** @type {Importer.page} */ content) => {
   );
 };
 
+const isDividend = (/** @type {Importer.page} */ content) => {
+  return content.indexOf('Dividendengutschrift') > 0;
+};
+
 export const canParseDocument = (
   /** @type {Importer.page[]} */ pages,
   /** @type {string} */ extension
@@ -114,19 +247,25 @@ export const canParseDocument = (
     firstPageContent.some((/** @type {string | string[]} */ line) =>
       line.includes('S Broker')
     ) &&
-    firstPageContent.some((/** @type {string | string[]} */ line) =>
-      line.includes('v2.7')
+    firstPageContent.some(
+      (/** @type {string | string[]} */ line) =>
+        line.includes('v2.7') || line.includes('v10.7')
     ) &&
     getDocumentType(firstPageContent) !== undefined
   );
 };
 
 export const parsePages = (/** @type {Importer.page[]} */ pages) => {
+  const flatContent = pages.flat();
   let activities = [];
 
   switch (getDocumentType(pages[0])) {
     case 'Buy':
-      activities.push(parseBuyDocument(pages.flat()));
+      activities.push(parseBuyDocument(flatContent));
+      break;
+
+    case 'Dividend':
+      activities.push(parseDividendDocument(flatContent));
       break;
 
     default:
